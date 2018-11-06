c28280407e1352e8e148e656ba9f3c6a32075dfb
Zachary <zackbarry13@gmail.com>
2018-11-06 15:04:24 -0800

    New constructor and add_end functions to allow int/string argument instead of ListNode*

---
+++ AlbumList.cpp
@@ -0,0 +1,56 @@
+#include "AlbumList.h"
+
+ListNode::ListNode(int year, std::string title)
+	: year{ year }, title{ title } {};
+
+AlbumList::AlbumList() {};
+
+AlbumList::AlbumList(ListNode *start)
+	: head{ start } {};
+
+AlbumList::AlbumList(int year, std::string album)
+	: head{ new ListNode(year, album) } {}
+
+void AlbumList::add_end(ListNode *album)
+{
+	// if AlbumList is empty, initialize the head
+	if (!head)
+	{
+		head = album;
+	}
+	else
+	{
+		ListNode *temp = head;
+		// navigate to the end of the list
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		// update pointers to reflect added ListNode
+		temp->next = album;
+		album->prev = temp;
+	}
+}
+
+void AlbumList::add_end(int year, std::string album)
+{
+	add_end(new ListNode(year, album));
+}
+
+void AlbumList::print()
+{
+	if (!head)
+	{
+		std::cout << "List is empty -- nothing to print." << std::endl;
+	}
+	else
+	{
+		ListNode* temp = head;
+		while (temp)
+		{
+			std::cout << "(" << temp->title << ", "
+				<< temp->year << ")\n";
+			temp = temp->next;
+		}
+	}
+}
---
+++ AlbumList.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef ALBUMLIST_H
+#define ALBUMLIST_H
+
+#include <string>
+#include "ArtistTree.h"
+
+class AlbumList;
+
+class ListNode {
+	friend class AlbumList;
+	int year;
+	std::string title;
+	ListNode *prev;
+	ListNode *next;
+public:
+	ListNode(int, std::string);
+};
+
+class AlbumList {
+	ListNode *head;
+public:
+	AlbumList();
+	AlbumList(ListNode*);
+	AlbumList(int, std::string);
+	void add_end(ListNode*);
+	void add_end(int, std::string);
+	void print();
+};
+
+#endif
