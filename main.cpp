17e2dfd16b8521ea355fc088762926b97209e75f
Zachary <zackbarry13@gmail.com>
2018-11-06 14:56:12 -0800

    Added tests for the new ArtistList constructors

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+	std::cout << "Test default tree constructor." << std::endl;
 	ArtistTree mytree;
 	mytree.print_inorder();
 	TreeNode* KM = new TreeNode("Kevin Morby");
@@ -16,6 +17,22 @@ int main()
 
 	std::cout << std::endl;
 
+	std::cout << "Test tree construction via TreeNode." << std::endl;
+	TreeNode *CS = new TreeNode("What So Not");
+	ArtistTree mytree1{ CS };
+	mytree1.insert(new TreeNode("Baauer"));
+	mytree1.print_inorder();
+
+	std::cout << std::endl;
+
+	std::cout << "Test tree construction via string and insert via string." << std::endl;
+	ArtistTree mytree2{ "John Craigie" };
+	mytree2.insert("The Shook Twins");
+	mytree2.print_inorder();
+
+	std::cout << std::endl;
+
+	std::cout << "Test AlbumList with defualt constructor." << std::endl;
 	AlbumList mylist;
 	mylist.print();
 	ListNode *CM = new ListNode(2017, "City Music");
@@ -23,6 +40,9 @@ int main()
 	mylist.add_end(new ListNode(2018, "Solace"));
 	mylist.add_end(new ListNode(2018, "Room Inside the World"));
 	mylist.print();
+
+	std::cout << std::endl;
+
 
 	std::cin.get();
 }
