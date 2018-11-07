ee2b2470ff2d7d15df0744ffc176066f2771c0f7
Zachary <zackbarry13@gmail.com>
2018-11-07 15:41:18 -0800

    Function to test if a point is within a rectangle (for checking user clicks)

--- GraphicsHelper.cpp
+++ GraphicsHelper.cpp
@@ -16,3 +16,11 @@ sf::Vector2f findPosition(std::vector<sf::RectangleShape> rects)
 	}
 	return sf::Vector2f{ xpos,ypos };
 }
+
+bool clickRect(sf::RectangleShape rect, sf::Vector2i click)
+{
+	return (rect.getPosition().x <= click.x
+		&& click.x <= rect.getPosition().x + rect.getSize().x
+		&& rect.getPosition().y <= click.y
+		&& click.y <= rect.getPosition().y + rect.getSize().y);
+}
--- GraphicsHelper.h
+++ GraphicsHelper.h
@@ -5,11 +5,16 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <iostream>
+#include <string>
 
 // given a vector of rectangle shapes, return a vector of two floats --- the x value
 //  being the sum of the x-sizes of the rectangles plus the x-position of the first
 //  rectangle; the y being the y-position of the first
 //  (this function is usefull for drawing rectanlges side by side)
 sf::Vector2f findPosition(std::vector<sf::RectangleShape>);
+
+// returns true if the point specified by the vector lies within the rectangle (border inclusive)
+bool clickRect(sf::RectangleShape, sf::Vector2i);
 
 #endif
