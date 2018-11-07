860e5206cef3e1f60071f55e75f5c3a3b55b6a5f
Zachary <zackbarry13@gmail.com>
2018-11-07 15:41:33 -0800

    Added buttons for album display and to toggle user input

--- main.cpp
+++ main.cpp
@@ -8,31 +8,58 @@
 
 int main()
 {
+	// create example MusicData structure
+	MusicData mymusic1;
+	mymusic1.add_artist("Duran Jones");
+	mymusic1.add_album("Duran Jones", 2018, "Duran Jones and the Indications");
+	mymusic1.add_artist("DjM Trio");
+	mymusic1.add_album("DjM Trio", 2018, "Cave Art Pt. 2");
+	mymusic1.add_album("DjM Trio", 2014, "Cave Art");
+	mymusic1.add_album("DjM Trio", 2010, "The Intrigue");
+
 	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
 
 	// Load font file for sf::Text objects
 	std::string font_file = "SourceSansPro.otf";	// font for Text Objects
 	sf::Font font;
-
 	if (!font.loadFromFile(font_file))
 	{
 		std::cerr << "Could not load " << font_file << " font file." << std::endl;
 	}
 
 	// create text object for displaying user input
-	sf::Text text;
-	text.setFont(font);
-	text.setCharacterSize(30);
-	text.setFillColor(sf::Color::Red);
+	sf::Text input_text;
+	input_text.setFont(font);
+	input_text.setCharacterSize(30);
+	input_text.setFillColor(sf::Color::Red);
 	std::string artist_str;
 
-	// create rectangle object for determining when to display user input
+	// create a place to display user input
 	sf::RectangleShape artist_rect(sf::Vector2f(200, 50));
+	// create a button to toggle whether or not input is accepted
 	sf::RectangleShape input_rect(sf::Vector2f(50, 50));
 	sf::Vector2f offset = findPosition({ artist_rect });
 	input_rect.setPosition(offset);
 	input_rect.setFillColor(sf::Color::Blue);
-	bool isSelected = false;
+	// create a button to pass the input to the MusicData class
+	sf::RectangleShape enter_rect(sf::Vector2f(50, 50));
+	offset = findPosition({ artist_rect, input_rect });
+	enter_rect.setPosition(offset);
+	enter_rect.setFillColor(sf::Color::Red);
+
+	// create a place to display album names
+	sf::RectangleShape album_display(sf::Vector2f(300, 50));
+	album_display.setPosition(sf::Vector2f(0, 50));
+	album_display.setFillColor(sf::Color::White);
+	sf::Text album_output;
+	album_output.setFont(font);
+	album_output.setCharacterSize(30);
+	album_output.setFillColor(sf::Color::Black);
+	album_output.setPosition(sf::Vector2f(0, 50));
+	std::string album_str;
+
+	bool inputOn = true;	// text gets entered at the start
+	bool displayAlbums = false;	// no albums to display yet
 
 	while (window.isOpen())
 	{
@@ -45,21 +72,38 @@ int main()
 				window.close();
 				break;
 			case sf::Event::MouseButtonReleased:
-				if (event.key.code == sf::Mouse::Left
-					&& sf::Mouse::getPosition(window).x >= input_rect.getPosition().x
-					&& sf::Mouse::getPosition(window).x <= input_rect.getPosition().x + input_rect.getSize().x)
+				if (event.key.code == sf::Mouse::Left)
 				{
-					isSelected = !isSelected;
+					// check if either button was clicked on; update boolean values accordingly
+					if (clickRect(input_rect, sf::Mouse::getPosition(window)))
+					{
+						inputOn = !inputOn;
+					}
+					else if (clickRect(enter_rect, sf::Mouse::getPosition(window)))
+					{
+						displayAlbums = !displayAlbums;
+						if (displayAlbums)
+						{
+							album_str = "House";
+							inputOn = false;
+						}
+						else
+						{
+							album_str = "";
+							inputOn = true;
+						}
+						album_output.setString(album_str);
+					}
 				}
 				break;
 			case sf::Event::TextEntered:
-				if (isSelected)
+				if (inputOn)
 				{
 					// handle ASCII characters only
 					if (event.text.unicode < 128)
 					{
 						artist_str += static_cast<char>(event.text.unicode);
-						text.setString(artist_str);
+						input_text.setString(artist_str);
 					}
 				}
 			}
@@ -67,9 +111,12 @@ int main()
 
 		window.clear();
 
+		window.draw(album_display);
 		window.draw(artist_rect);
 		window.draw(input_rect);
-		window.draw(text);
+		window.draw(enter_rect);
+		window.draw(input_text);
+		window.draw(album_output);
 
 		window.display();
 	}
