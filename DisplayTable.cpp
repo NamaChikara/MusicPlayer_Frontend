db886e1c020fa8f52549c186e6139aa18172591f
Zachary <zackbarry13@gmail.com>
2018-11-08 08:18:12 -0800

    Graphic table to display a list of strings (which will be artist names or album titles in this program)

---
+++ DisplayTable.cpp
@@ -0,0 +1,29 @@
+#include "DisplayTable.h"
+
+DisplayTable::DisplayTable(std::string font_file)
+{
+	font.loadFromFile(font_file);
+}
+
+void DisplayTable::update(std::vector<std::string> labels)
+{
+	texts.clear();
+	rects.clear();
+	for (int i = 0; i < labels.size(); ++i)
+	{
+		rects.push_back(new sf::RectangleShape(sf::Vector2f(200, 50)));
+		rects[i]->setFillColor(sf::Color::Black);
+		if (i > 0)
+		{
+			rects[i]->setPosition(botLeft(rects[i - 1]));
+		}
+		texts.push_back(new sf::Text(labels[i], font, charsize));
+		texts[i]->setFillColor(sf::Color::White);
+		texts[i]->setPosition(rects[i]->getPosition());
+	}
+}
+
+void DisplayTable::draw(sf::RenderTarget&, sf::RenderStates) const
+{
+
+}
---
+++ DisplayTable.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef DisplayTable_H
+#define DisplayTable_H
+
+#include <SFML/Graphics.hpp>
+#include "GraphicsHelper.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+class DisplayTable
+{
+public:
+	// need to load font file when creating a DisplayTable
+	DisplayTable(std::string);
+
+	// new strings to display; one Text/RectangleShape for each string
+	void update(std::vector<std::string>);	
+
+	// override sf::Drawable's pure virtual function
+	virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+
+private:
+	sf::Font font;
+	float charsize = 20;
+	std::vector<sf::Text*> texts;
+	// one RectangleShape for each Text element to allow for alternating
+	//  background colors if desired
+	std::vector<sf::RectangleShape*> rects;
+};
+
+
+
+#endif DisplayTable_H
