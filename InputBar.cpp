67a858951ab3c9a1bf27f0284f5d5b637e239600
Zachary <zackbarry13@gmail.com>
2018-11-07 20:31:42 -0800

    Static bar that will be updated to allow user input and program response

---
+++ InputBar.cpp
@@ -0,0 +1,85 @@
+#include "InputBar.h"
+
+InputBar::InputBar(std::string font_file)
+{
+	font.loadFromFile(font_file);
+
+	place_rects();
+	setup_texts();
+	text_locations();
+}
+
+void InputBar::draw(sf::RenderTarget& target, sf::RenderStates) const
+{
+	target.draw(artist_rect);
+	target.draw(album_rect);
+	target.draw(year_rect);
+	target.draw(set_rect);
+	target.draw(get_rect);
+
+	target.draw(artist_text);
+	target.draw(album_text);
+	target.draw(year_text);
+	target.draw(set_text);
+	target.draw(get_text);
+}
+
+void InputBar::place_rects()
+{
+	artist_rect.setSize(sf::Vector2f{ 100,rect_height });
+	artist_rect.setFillColor(sf::Color::Blue);
+
+	album_rect.setSize(sf::Vector2f{ 100,rect_height });
+	album_rect.setPosition(topRight(artist_rect));
+	album_rect.setFillColor(sf::Color::Red);
+
+	year_rect.setSize(sf::Vector2f{ 75,rect_height });
+	year_rect.setPosition(topRight(album_rect));
+	year_rect.setFillColor(sf::Color::Blue);
+
+	set_rect.setSize(sf::Vector2f{ 50,rect_height });
+	set_rect.setPosition(topRight(year_rect));
+	set_rect.setFillColor(sf::Color::Red);
+
+	get_rect.setSize(sf::Vector2f{ 50,rect_height });
+	get_rect.setPosition(topRight(set_rect));
+	get_rect.setFillColor(sf::Color::Blue);
+}
+
+void InputBar::setup_texts()
+{
+	artist_text.setFont(font);
+	artist_text.setCharacterSize(charsize);
+	artist_text.setString(artist_init);
+	artist_text.setFillColor(sf::Color(255, 255, 255));
+
+	album_text.setFont(font);
+	album_text.setString(album_init);
+	album_text.setCharacterSize(charsize);
+	artist_text.setFillColor(sf::Color(255, 255, 255));
+
+	year_text.setFont(font);
+	year_text.setString(year_init);
+	year_text.setCharacterSize(charsize);
+	artist_text.setFillColor(sf::Color(255, 255, 255));
+
+	set_text.setFont(font);
+	set_text.setString(set_init);
+	set_text.setCharacterSize(charsize);
+	artist_text.setFillColor(sf::Color(255, 255, 255));
+
+	get_text.setFont(font);
+	get_text.setString(get_init);
+	get_text.setCharacterSize(charsize);
+	artist_text.setFillColor(sf::Color(255, 255, 255));
+}
+
+void InputBar::text_locations()
+{
+	float voffset = (rect_height - (float)charsize) / 2;
+	artist_text.setPosition(artist_rect.getPosition() + sf::Vector2f(hoffset, voffset));
+	album_text.setPosition(album_rect.getPosition() + sf::Vector2f(hoffset, voffset));
+	year_text.setPosition(year_rect.getPosition() + sf::Vector2f(hoffset, voffset));
+	set_text.setPosition(set_rect.getPosition() + sf::Vector2f(hoffset, voffset));
+	get_text.setPosition(get_rect.getPosition() + sf::Vector2f(hoffset, voffset));
+}
---
+++ InputBar.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifndef InputBar_H
+#define InputBar_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+#include "GraphicsHelper.h"
+
+class InputBar : public sf::Drawable
+{
+public:
+	InputBar(std::string);
+
+	// override sf::Drawable's pure virtual function
+	virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+
+private:
+	sf::Font font;	// must load a font to draw Text objects
+	int charsize = 20;	// how large text should be
+	float rect_height = 50;	// how tall the RectangleShapes are
+	float hoffset = 5; // horizontal offset of text within rectangle
+
+	// initial string values for text objects
+	std::string artist_init = "artist";
+	std::string album_init = "album";
+	std::string year_init = "year";
+	std::string set_init = "set";
+	std::string get_init = "get";
+
+	// Text objects will be placed inside the RectangleShapes
+	sf::Text artist_text;
+	sf::Text album_text;
+	sf::Text year_text;
+	sf::Text set_text;
+	sf::Text get_text;
+
+	sf::RectangleShape artist_rect;
+	sf::RectangleShape album_rect;
+	sf::RectangleShape year_rect;
+	sf::RectangleShape set_rect;
+	sf::RectangleShape get_rect;
+
+	void place_rects();		// set the widths of the rectangles
+	void setup_texts();		// set Text objects' initial size/string parameters
+	void text_locations();   // set texts within the rectangles
+};
+
+
+#endif InputBar_H
