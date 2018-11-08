45599d4a5d5a6806a964acc6a79c1592aea5f50d
Zachary <zackbarry13@gmail.com>
2018-11-08 08:18:35 -0800

    Reflect changes made to InputBar's draw function

--- main.cpp
+++ main.cpp
@@ -45,40 +45,11 @@ int main()
 
 		window.clear();
 
-		myinput.draw(window, sf::RenderStates());
+		myinput.draw(window);
 
 		window.display();
 	}
-	/*
-	// create text object for displaying user input
-	sf::Text input_text;
-	input_text.setFont(font);
-	input_text.setCharacterSize(30);
-	input_text.setFillColor(sf::Color::Red);
-	std::string artist_str;
-
-	// create a place to display user input
-	sf::RectangleShape artist_rect(sf::Vector2f(200, 50));
-	// create a button to toggle whether or not input is accepted
-	sf::RectangleShape input_rect(sf::Vector2f(50, 50));
-	input_rect.setPosition(topRight(artist_rect));
-	input_rect.setFillColor(sf::Color::Blue);
-	// create a button to pass the input to the MusicData class
-	sf::RectangleShape enter_rect(sf::Vector2f(50, 50));
-	enter_rect.setPosition(topRight(input_rect));
-	enter_rect.setFillColor(sf::Color::Red);
-
-	// create a place to display album names
-	sf::RectangleShape album_display(sf::Vector2f(300, 50));
-	album_display.setPosition(sf::Vector2f(0, 50));
-	album_display.setFillColor(sf::Color::White);
-	sf::Text album_output;
-	album_output.setFont(font);
-	album_output.setCharacterSize(30);
-	album_output.setFillColor(sf::Color::Black);
-	album_output.setPosition(sf::Vector2f(0, 50));
-	std::string album_str;
-
+/*
 	bool inputOn = true;	// text gets entered at the start
 	bool displayAlbums = false;	// no albums to display yet
 
